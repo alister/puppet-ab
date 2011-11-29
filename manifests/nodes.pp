0b7f25b7718a45b06cc02fe81eae469aacbc3efe
Alister Bulman <abulman@gmail.com>
2011-11-29 17:06:50 +0000

    add other classes, and also node types

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,8 +1,30 @@
 node base {
    include ntp
-   include ack_grep
    include joe
+   include git
+   include pigz
+}
+
+node development inherits base {
+   include ack_grep
+   include build
+   include python
+   include rake 
+}
+
+node serverQueue inherits base {
+    #beanstalkd 
+}
+node serverMemcache inherits base {
+    #include memcached
+}
+node serverWorkers inherits base {
+    #include supervisord
 }
 
 node oberth inherits base {
+}
+
+
+node puppetc1 inherits base {
 }

dcc1945496b0c9f128c832b9800c4d37cc4dfe37
Alister Bulman <abulman@gmail.com>
2012-05-07 13:48:38 +0100

    puppet-lint'ed

--- modules/basesw/manifests/alister/sublimetext2.pp
+++ modules/basesw/manifests/alister/sublimetext2.pp
@@ -1,34 +1,34 @@
 /**
- * Plugins that I have installed by default
- * 
- * I've not actually used all of them yet....
- */
+* Plugins that I have installed by default
+*
+* I've not actually used all of them yet....
+*/
 class basesw::alister::sublimetext2 {
 
   # these must be set
-  class { sublimetext2::params:
+  class { 'sublimetext2::params':
     userName        => 'alister',
-    installBasePath => "/home/alister/.config/sublime-text-2/Packages"
-    # @TODO given the userName, and the OS, we could probably work out the 
-    # installBasePath in ::params ??
+    installBasePath => '/home/alister/.config/sublime-text-2/Packages'
+    # @TODO given the userName, and the OS, we could probably
+    # work out the installBasePath in ::params ??
   }
   # also required
   include sublimetext2
 
   sublimetext2::plugin { 'DocBlockr':
-    gitUrl     => 'https://github.com/spadgos/sublime-jsdocs.git'
+    gitUrl => 'https://github.com/spadgos/sublime-jsdocs.git'
   }
-  sublimetext2::plugin { text-2-ini:
-    gitUrl     => 'https://github.com/clintberry/sublime-text-2-ini.git'
+  sublimetext2::plugin { 'text-2-ini':
+    gitUrl => 'https://github.com/clintberry/sublime-text-2-ini.git'
   }
   sublimetext2::plugin { 'LineEndings':
-    gitUrl     => 'https://github.com/SublimeText/LineEndings.git'
+    gitUrl => 'https://github.com/SublimeText/LineEndings.git'
   }
   sublimetext2::plugin { 'SublimeCodeIntel':
-    gitUrl     => 'https://github.com/Kronuz/SublimeCodeIntel.git'
+    gitUrl => 'https://github.com/Kronuz/SublimeCodeIntel.git'
   }
   sublimetext2::plugin { 'ToggleQuotes':
-    gitUrl     => 'https://github.com/spadgos/sublime-ToggleQuotes.git'
+    gitUrl => 'https://github.com/spadgos/sublime-ToggleQuotes.git'
   }
   sublimetext2::plugin { 'SmartMatch':
     gitUrl => 'git@github.com:ccampbell/sublime-smart-match.git'

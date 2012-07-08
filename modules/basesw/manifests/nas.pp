16b96a5e4efc43ef64ad0d2acc75616ca623571d
Alister Bulman <abulman@gmail.com>
2012-07-08 11:59:51 +0100

    more

--- modules/basesw/manifests/nas.pp
+++ modules/basesw/manifests/nas.pp
@@ -2,16 +2,26 @@ class basesw::nas {
 
   #include app::phpmyadmin
 
-  $sw = [ 'rsnapshot' ]
+  $sw = [ 'rsnapshot', 'sabnzbdplus' ]
   package { $sw:
     ensure => installed,
   }
 
   $fortune = ['fortune-mod', 'fortunes', 'fortunes-bofh-excuses',
-    'fortunes-debian-hints', 'fortunes-min', 'fortunes-off',
-    'fortunes-ubuntu-server']
+    'fortunes-min', 'fortunes-off',
+  ]
   package { $fortune:
     ensure => installed,
+  }
+  package { [ 'fortunes-debian-hints', 'fortunes-ubuntu-server']:
+    ensure => purged
+  }
+  
+  # these are listed as Alias's in the portal.alister apache.conf repo
+  $phpPkgs = [ 'phpsysinfo', 'phpmyadmin' ]
+  package { $phpPkgs:
+    ensure => installed,
+    #notify => Service['apache']
   }
 
 }
